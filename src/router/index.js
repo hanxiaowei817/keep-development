c0cbef767dcb3f307c8cb89d8b7f1c0ef6a8e2d6
hanxiaowei <1790897865@qq.com>
2021-02-04 16:18:39 +0800

    keepdemo

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+//搭建路由
+const router = new VueRouter({
+    mode: 'hash',
+    routes: [
+        {
+            path: '/',
+            redirect: '/pagehome' //默认跳转路径
+
+        },
+        {
+            path: '/pagehome',//路由级组件
+            component: () => import('../views/pagehome/index.vue')
+
+        },
+        {
+            path: '/plan',//路由级组件
+            component: () => import('../views/plan/index.vue')
+
+        },
+        {
+            path: '/motion',//路由级组件
+            component: () => import('../views/motion/index.vue')
+
+        },
+        {
+            path: '/shopping',//路由级组件
+            component: () => import('../views/shopping/index.vue')
+
+        },
+        {
+            path: '/my',//路由级组件
+            component: () => import('../views/my/index.vue')
+
+        },
+    ],
+    linkActiveClass: 'active'
+})
+
+export default router
